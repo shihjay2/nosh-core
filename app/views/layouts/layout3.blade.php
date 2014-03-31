c1054c186559763748a5ac5dfd8849ab2185fcf3
Michael Shihjay Chen <shihjay2@gmail.com>
2014-03-31 09:41:59 -0700

    Missing cookies plugin.

--- app/views/layouts/layout3.blade.php
+++ app/views/layouts/layout3.blade.php
@@ -109,6 +109,8 @@
 		<?php echo HTML::script('js/jquery.populate.js'); ?>
 		<?php echo HTML::script('js/jquery.ocupload.js'); ?>
 		<?php echo HTML::script('js/main.js'); ?>
+		<?php echo HTML::script('js/jstz-1.0.4.min.js'); ?>
+		<?php echo HTML::script('js/jquery.cookie.js'); ?>
 		<?php echo HTML::script('js/bluebutton.js'); ?>
 		{{ $script }}
 	</head>
