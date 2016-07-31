cfba36b53b4936d50ba2d775cb736ce897e3d609
Michael Shihjay Chen <shihjay2@gmail.com>
2016-07-30 22:25:02 -0700

    Update reset routine.

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -884,9 +884,11 @@ class LoginController extends BaseController {
 		$config_file = __DIR__."/../../.env.php";
 		$config = require($config_file);
 		$file = '/noshdocuments/demo.sql';
-		$file1 = '/noshdocuments/demo_oic.sql';
+		$file1 = '/noshdocuments/demo_oidc.sql';
+		// $file1 = '/noshdocuments/demo_oic.sql';
 		$command = "mysql -u " . $config['mysql_username'] . " -p". $config['mysql_password'] . " nosh < " . $file;
-		$command1 = "mysql -u " . $config['mysql_username'] . " -p". $config['mysql_password'] . " oic_production < " . $file1;
+		// $command1 = "mysql -u " . $config['mysql_username'] . " -p". $config['mysql_password'] . " oic_production < " . $file1;
+		$command1 = "mysql -u " . $config['mysql_username'] . " -p". $config['mysql_password'] . " oidc < " . $file1;
 		system($command);
 		system($command1);
 		return Redirect::to('/');
